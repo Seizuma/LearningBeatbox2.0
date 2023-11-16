a26ee557711bedc3296bdd9e88c77c0224d39eaf
Seizuma <stanleguen.pro@gmail.com>
2023-11-16 09:52:17 +0100

    ADD new sounds Change gitignore Track large files

--- data/code/mode_tutorial_a.py
+++ data/code/mode_tutorial_a.py
@@ -46,8 +46,8 @@ class TutorialMode(BaseMode):
             'name': 'm',
             'sounds': ['m'],
             'threshold': {
-                'power': 100000,
-                'percentage': 75,
+                'power': 80000,
+                'percentage': 80,
             },
             'throttle': {
                 'm': 0.1
@@ -82,6 +82,47 @@ class TutorialMode(BaseMode):
             'throttle': {
                 'lips oscilation (from pash kick)': 0.1
             }
+        },
+        {
+            'name': 'Fart bass (villain)',
+            'sounds': ['Fart bass (villain)'],
+            'threshold': {
+                'power': 180000,
+                'percentage': 90,
+            },
+            'continual_threshold': {
+                'power': 20000,
+                'percentage': 60,
+            },
+            'throttle': {
+                'Fart bass (villain)': 0.1
+            }
+        },
+        {
+            'name': 'Vocalized lips oscilation',
+            'sounds': ['Vocalized lips oscilation'],
+            'threshold': {
+                'power': 180000,
+                'percentage': 90,
+            },
+            'continual_threshold': {
+                'power': 20000,
+                'percentage': 60,
+            },
+            'throttle': {
+                'Vocalized lips oscilation': 0.1
+            }
+        },
+        {
+            'name': 'pressurized hi hat',
+            'sounds': ['pressurized hi hat'],
+            'threshold': {
+                'power': 40000,
+                'percentage': 90,
+            },
+            'throttle': {
+                'pressurized hi hat': 0.1
+            }
         }
     ]
 
@@ -98,4 +139,101 @@ class TutorialMode(BaseMode):
             self.press('o')
         elif (self.detect('lips oscilation (from pash kick)')):
             self.press('v')
+        elif (self.detect('Fart bass (villain)')):
+            self.press('f')
+        elif (self.detect('Vocalized lips oscilation')):
+            self.press('v')
+            self.press('e')
+            self.press('u')
+        elif (self.detect('pressurized hi hat')):
+            self.press('t')
+            self.press('s')
         return
+
+
+"""
+patternsDiscrete = [
+        {
+            'name': 'b',
+            'sounds': ['b'],
+            'threshold': {
+                'power': 155000,
+                'frequency': 34,
+                'percentage': 77,
+                'below_frequency': 40,
+            },
+            'throttle': {
+                'b': 0.1
+            }
+        },
+        {
+            'name': 'k',
+            'sounds': ['k'],
+            'threshold': {
+                'power': 75000,
+                'percentage': 95,
+                # 'frequency': 40,
+            },
+            'throttle': {
+                'k': 0.1
+            }
+        },
+        {
+            'name': 'hi hat',
+            'sounds': ['hi hat'],
+            'threshold': {
+                'power': 60000,
+                'percentage': 90,
+            },
+            'throttle': {
+                'hi hat': 0.05
+            }
+        },
+        {
+            'name': 'm',
+            'sounds': ['m'],
+            'threshold': {
+                'power': 80000,
+                'percentage': 80,
+            },
+            'throttle': {
+                'm': 0.1
+            }
+        },
+    ],
+
+    patternsContinuous = [
+        {
+            {
+                'name': 'throat bass',
+                        'sounds': ['throat bass'],
+                        'threshold': {
+                            'power': 100000,
+                            'percentage': 90,
+                        },
+                'continual_threshold': {
+                            'power': 20000,
+                            'percentage': 60,
+                        },
+                'throttle': {
+                            'throat bass': 0.1
+                        }
+            },
+            {
+                'name': 'lips oscilation (from pash kick)',
+                'sounds': ['lips oscilation (from pash kick)'],
+                'threshold': {
+                    'power': 180000,
+                    'percentage': 90,
+                },
+                'continual_threshold': {
+                    'power': 20000,
+                    'percentage': 60,
+                },
+                'throttle': {
+                    'lips oscilation (from pash kick)': 0.1
+                }
+            }
+        }
+    ] 
+    """
