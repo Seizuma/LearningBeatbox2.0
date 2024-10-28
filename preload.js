7b907869dbfeeeaada58bf0f3f3756e8bb842f8e
Seizuma <82123001+Seizuma@users.noreply.github.com>
2024-10-28 23:33:27 +0100

    WIP: backing up to rework project

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    startPython: () => ipcRenderer.send('start-python'),
-    stopPython: () => ipcRenderer.send('stop-python'),
-    onPythonData: (callback) => ipcRenderer.on('python-data', (event, data) => callback(data)),
-});
